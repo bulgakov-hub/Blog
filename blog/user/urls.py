0d029fe65b1cc598179f5215154827387fed07de
bulgakov-hub <bulgakov@DESKTOP-5G1521E.localdomain>
2020-10-26 10:41:49 +0300

    Create app user, login, logout

--- blog/user/urls.py
+++ blog/user/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
+
+app_name = 'user'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('user.urls', namespace='user')),
+    path('', LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='user/logout.html'), name='logout'),
 ]
